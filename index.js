758a0c5463285b6a2ebbb34d413e7269ccf9b50f
Jada Xex <jada.xex@gmail.com>
2020-12-15 18:31:52 +0100

    Basic styling and fixing time.

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var usr = "";
 
 function getUserName(){
 
-  var userName = window.prompt('Welcome to this cozy chatroom. \nWhat should everyone call you?', "");
+  var userName = window.prompt('Welcome to our cozy chatroom. \nWhat should everyone call you?', "");
 
   /* check input length */
   if (userName.length > 0 && userName.length < 20){
@@ -33,18 +33,43 @@ async function getMessages(){
 function visualizeMessages(data_stream){
 
   for (let i = 0; i < data_stream.length; i++){
-    var node = document.createElement("li");
+    var node = document.createElement("div");
+    var att = document.createAttribute("class");       // Create a "class" attribute
+    att.value = "bubble";
+    node.setAttributeNode(att);
+
+    var author_container = document.createElement("div");
+    var author_attribute = document.createAttribute("class");
+    author_attribute.value = "author";
+    author_container.setAttributeNode(author_attribute);
+    /* get and set value from data */
     var author = document.createTextNode(data_stream[i].author);
+    author_container.appendChild(author);
+    node.appendChild(author_container);
+
 
     //format for visualization
     var formattedTime = convertTimestamp(data_stream[i].timestamp);
     var formattedMessage = checkHTMLencode(data_stream[i].message);
 
-    var time = document.createTextNode(formattedTime);         // Create a text node
+    var message_container = document.createElement("div");
+    var message_attribute = document.createAttribute("class");
+    message_attribute.value = "message";
+    message_container.setAttributeNode(message_attribute);
     var message = document.createTextNode(formattedMessage);
-    node.appendChild(author);
-    node.appendChild(message);
-    node.appendChild(time);
+    message_container.appendChild(message);
+    node.appendChild(message_container);
+    /* get and set value from data */
+
+    var time_container = document.createElement("div");
+    var time_attribute = document.createAttribute("class");
+    time_attribute.value = "time";
+    time_container.setAttributeNode(time_attribute);
+    var time = document.createTextNode(formattedTime);
+    time_container.appendChild(time);
+    node.appendChild(time_container);
+
+
     document.getElementById('Message Screen').appendChild(node);
   }
 }
@@ -74,24 +99,31 @@ async function updateMessage(){
 
 function convertTimestamp(stamp){
 
-  var fullDate = new Date(stamp * 1000);
+  var fullDate = new Date(stamp);
+  console.log(stamp);
+  console.log(fullDate);
 
-  var days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+//  var days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-  var day = days[fullDate.getDay()];
+//  var day = days[fullDate.getDay()];
 
-  var month = fullDate.getMonth();
+  var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+  var month = months[fullDate.getMonth()];
+
+  var year = fullDate.getFullYear();
 
   var date = fullDate.getDate();
 
   var hours = fullDate.getHours();
+
 
   var minutes = "0" + fullDate.getMinutes();
 
   //    var seconds = "0" + date.getSeconds();
 
   // Will display time in weekday month/day \n 10:30 format
-  var formattedTime = hours + ':' + minutes.substr(-2) + " " + day + " " + month + "/" + date;
+  var formattedTime = date + " " + month + " " + year + " " + hours + ':' + minutes.substr(-2);
 
   return formattedTime;
 
@@ -168,4 +200,6 @@ async function send_n_refresh(){
   await sendMessage();
   updateMessage();
 }
+
+//https://www.digitalocean.com/community/tutorials/js-async-functions
 */

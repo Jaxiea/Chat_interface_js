a8f407212bfc1cb257a60ef78cc505175043560b
Jada Xex <jada.xex@gmail.com>
2020-12-16 13:04:26 +0100

    Fixed send_n_update() feature. Fixed author bubble float right feature

--- index.js
+++ index.js
@@ -30,6 +30,93 @@ async function getMessages(){
   visualizeMessages(known_data_stream);
 }
 
+getUserName();
+
+
+/* updateMessage() takes the entire current message history and compares it to the saved messages client-side. Inefficient. */
+/* Given a bit more time, I'd implement a getMessagesBy(timestamp); and compare a set length of data streams, which can also ensure the delay()
+used to levy the send and retrieve time to be always working. */
+
+async function updateMessage(){
+
+  const response = await fetch(AllMessagesURL);
+  const data = await response.json();
+
+  var current_data_stream = data;
+
+//  console.log(known_data_stream);
+//  console.log(data);
+
+/* gotta slice the array correctly. */
+  var new_data = current_data_stream.slice(known_data_stream.length, current_data_stream.length);
+//  console.log("newdata", new_data);
+
+  visualizeMessages(new_data);
+  known_data_stream = current_data_stream;
+}
+
+
+function sendMessage(){
+  const timestamp = Date.now();
+  var txt = document.getElementById('Input_Box').value;
+  txt = txt.replace("'", "&lsquo;");
+
+  if (txt.length < 1){
+    alert("Void is not allowed here.");
+    return;
+  }
+
+  const messageObj = {
+    message:txt,
+    author:usr,
+  }
+
+  const options = {
+    method: 'POST',
+    body: JSON.stringify(messageObj),
+    headers: {
+      'Content-Type': 'application/json',
+      'token': '4hbLloOBHnKP',
+    }
+  }
+
+  fetch(POSTurl, options)
+    .then(res => res.json())
+    .then(res => console.log(res))
+    .catch(err => console.log(err));
+
+}
+
+/* Didn't figure this out in time */
+
+
+function send_n_refresh(){
+  sendMessage();
+
+  delay(2000)      /* 2000 won't always work, see comment above updateMessage(). */
+  .then(() => {
+    updateMessage();
+    console.log("delayed!");
+  })
+  .catch((err) => console.error(err));
+
+}
+
+/* Wait for the message to be sent then refresh list */
+function delay(time) {
+  return new Promise((resolve, reject) => {
+    if (isNaN(time)) {
+      reject(new Error('delay requires a valid number'))
+    } else {
+      setTimeout(resolve, time);
+    }
+  });
+}
+
+//https://www.digitalocean.com/community/tutorials/js-async-functions
+
+
+
 function visualizeMessages(data_stream){
 
   for (let i = 0; i < data_stream.length; i++){
@@ -44,11 +131,14 @@ function visualizeMessages(data_stream){
     author_container.setAttributeNode(author_attribute);
     /* get and set value from data */
     var author = document.createTextNode(data_stream[i].author);
+    var tmp = data_stream[i].author;
+    var tmp_usr = String(usr);
+
     author_container.appendChild(author);
     node.appendChild(author_container);
 
 
-    //format for visualization
+    //formatting for visualization, time and message
     var formattedTime = convertTimestamp(data_stream[i].timestamp);
     var formattedMessage = checkHTMLencode(data_stream[i].message);
 
@@ -71,37 +161,27 @@ function visualizeMessages(data_stream){
 
 
     document.getElementById('Message_Screen').appendChild(node);
+
+    if (tmp == tmp_usr){
+      att.value = "bubble_me";
+      console.log(att.value);
+      node.setAttributeNode(att);
+      var clear_div = document.createElement("div");
+      clear_div.style.clear = 'both';
+      document.getElementById('Message_Screen').appendChild(clear_div);
+    }
   }
+
+  document.getElementById("Input_Box").focus();
+  /* Not implemented. Focus on the latest message. */
 }
-
-getUserName();
-
-
-async function updateMessage(){
-
-  const response = await fetch(AllMessagesURL);
-  const data = await response.json();
-
-  var current_data_stream = data;
-
-//  console.log(known_data_stream);
-//  console.log(data);
-
-/* gotta slice the array correctly. */
-  var new_data = current_data_stream.slice(known_data_stream.length, current_data_stream.length);
-//  console.log("newdata", new_data);
-
-  visualizeMessages(new_data);
-  known_data_stream = current_data_stream;
-}
-
 
 
 function convertTimestamp(stamp){
 
   var fullDate = new Date(stamp);
-  console.log(stamp);
-  console.log(fullDate);
+//  console.log(stamp);
+//  console.log(fullDate);
 
 //  var days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
@@ -160,46 +240,3 @@ function checkHTMLencode(txt){
 
   return str;
 }
-
-
-function sendMessage(){
-  const timestamp = Date.now();
-  var txt = document.getElementById('Input_Box').value;
-  txt = txt.replace("'", "&lsquo;");
-
-  if (txt.length < 1){
-    alert("Void is not allowed here.");
-    return;
-  }
-
-  const messageObj = {
-    message:txt,
-    author:usr,
-  }
-
-  const options = {
-    method: 'POST',
-    body: JSON.stringify(messageObj),
-    headers: {
-      'Content-Type': 'application/json',
-      'token': '4hbLloOBHnKP',
-    }
-  }
-
-  fetch(POSTurl, options)
-  .then(res => res.json())
-  .then(res => console.log(res));
-
-}
-
-
-/* Didn't figure this out in time */
-
-/*
-async function send_n_refresh(){
-  await sendMessage();
-  updateMessage();
-}
-
-//https://www.digitalocean.com/community/tutorials/js-async-functions
-*/
